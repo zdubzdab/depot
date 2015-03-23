6ea8ac4dbd0599e35688a6622c505d126f68375e
zdubzdab <zdubzdab@gmail.com>
2015-03-23 12:26:31 +0200

    user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   has_many :products, dependent: :destroy
   has_many :line_items, through: :products
   has_many :orders, through: :line_items
-  
+
+  validates :name, presence: true,
+                    length: { minimum: 3 },
+                    uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
