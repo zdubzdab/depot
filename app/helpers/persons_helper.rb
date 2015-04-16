351d099e8992ca3cf8424401e106bd7cc7c65fbc
zdubzdab <zdubzdab@gmail.com>
2015-04-16 11:13:06 +0300

    before rspec

--- app/helpers/persons_helper.rb
+++ app/helpers/persons_helper.rb
@@ -1,21 +1,11 @@
 module PersonsHelper
   def new_orders (array)
     array2 = []
-    array3 = []
     array.each do |i|
-      array2.push(i.status)
+      array2 << (i.status)
     end
-      array2.each do |i|
-        if i == "new"
-          array3.push(i)
-        else
-        end
-      end
 
-      if array3.empty?
-        0
-      else
-        array3.count
-      end
+    array2.count { |i| i == "new"}
   end
 end
+
